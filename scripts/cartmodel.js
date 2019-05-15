57ef0625cd8869fa96d209ff647329af4c70678d
Harerakalex <hareraloston@gmail.com>
2019-05-15 09:06:19 +0100

    Adding sign up form and setting up project folder

---
+++ scripts/cartmodel.js
@@ -0,0 +1,21 @@
+var modal = document.getElementById('myCart');
+
+var btn = document.getElementById("cartBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
